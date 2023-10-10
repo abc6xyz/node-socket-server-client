e1f3514ea1cc76e9e0866d86f2d981a8f8acc17a
abc6xyz <byte11101100@gmail.com>
2023-10-10 15:21:10 -0700

    update app.js

--- app.js
+++ app.js
@@ -1,65 +1,51 @@
-const WebSocket = require('ws');
+const WebSocket = require('ws')
 
-const wss = new WebSocket.Server({ port: 8000 });
-const A_MAC = '00:00:00:00:00:00'
+const wss = new WebSocket.Server({ port: 8000 })
+const A_MAC = '00-00-00-00-00-00'
 
 let clients = {}
 
 wss.on('connection', (ws, req) => {
-  const data = req.url.split('/');
-  let mac;
+  const data = req.url.split('/')
+  let mac
   if (data[1] === 'a' && data[2] === '2001623'){
     mac = A_MAC
-    clients[mac] = ws;
+    clients[mac] = ws
   }
   else if (data[1] === 'b'){
     if(clients.hasOwnProperty(data[2])){
-      ws.send('double connection');
       return
     }
-    mac = data[2];
-    clients[mac] = ws;
+    mac = data[2]
+    clients[mac] = ws
   }
   else{
-    ws.close();
+    ws.close()
   }
   
   ws.on('message', (message) => {
-    const data = JSON.parse(message.toString());
-    if(data['to'] === "server"){
-      if(data['command'] === 'clients'){
-        let macs = []
-        for (const key in clients) {
-          macs.push(key)
-        }
-        ws.send(JSON.stringify(macs))
-      } else {
-        ws.send("unknown command")
-      }
-    } else if(data['from'] === A_MAC) {
-      if(data['to'] === A_MAC){
-        for (const key in clients) {
-          if (key === A_MAC) continue
-          clients[key].send(message)
+    const data = JSON.parse(message.toString())
+    try {
+      if(data['to'] === "server"){
+        if(data['message'] === 'clients'){
+          let macs = []
+          for (const key in clients) {
+            macs.push(key)
+          }
+          ws.send(JSON.stringify(macs))
+        } else {
+          ws.send(JSON.stringify({"message":"unknown command"}))
         }
       } else {
-        try {
-          clients[data['to']].send(message)
-        } catch (error) {
-          ws.send('send error')          
-        }
+        clients[data['to']].send(message)
       }
-    } else {
-      try {
-        clients[A_MAC].send(message)
-      } catch (error) {
-        ws.send('send error')
-      }
+    } catch (error) {
+      console.log(error)
     }
-  });
+  })
 
   ws.on('close', () => {
     delete(clients[mac])
-    console.log(`Client disconnected ${mac}`);
-  });
-});
+    console.log(`Client disconnected ${mac}`)
+  })
+})
