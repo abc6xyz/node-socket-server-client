40c7983a4043b722ddc8510be43daa1e6b461f98
abc6xyz <byte11101100@gmail.com>
2023-11-29 11:43:43 +0800

    update url

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const WebSocket = require('ws')
 
 const wss = new WebSocket.Server({ port: 8000 })
 const A_MAC = 'uspy'
-const SERVER_URL = 'http://95.217.64.28:5000'
+const SERVER_URL = ''
 let clients = {}
 
 function heartbeat() {
